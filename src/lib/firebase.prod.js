7494a708638e8a2487e7583ddaf6eb846c5ab978
Will Kelly <willkelly9617@gmail.com>
2021-04-24 18:28:37 -0500

    dev branch

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -6,15 +6,26 @@ import 'firebase/auth';
 // !2: Seeded on Friday December 18, 2020 10:35AM: Do no uncomment
 // import { seedDatabase } from '../seed';
 
+// const config = {
+//   apiKey: process.env.apiKey,
+//   authDomain: process.env.authDomain,
+//   databaseURL: process.env.databaseURL,
+//   projectId: process.env.projectId,
+//   storageBucket: process.env.storageBucket,
+//   messagingSenderId: process.env.messagingSenderId,
+//   appId: process.env.appId,
+//   measurementId: process.env.measurementId,
+// };
+
 const config = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  databaseURL: process.env.databaseURL,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
-  measurementId: process.env.measurementId,
+  apiKey: 'AIzaSyCfL5KEXchYugUETvb_qLnCwNbkyuILPjI',
+  authDomain: 'netflix-clone-a1dcc.firebaseapp.com',
+  databaseURL: 'https://netflix-clone-a1dcc-default-rtdb.firebaseio.com',
+  projectId: 'netflix-clone-a1dcc',
+  storageBucket: 'netflix-clone-a1dcc.appspot.com',
+  messagingSenderId: '652838246696',
+  appId: '1:652838246696:web:70f60e884370622e9a181d',
+  measurementId: 'G-08TFKB7D6R',
 };
 
 const firebase = Firebase.initializeApp(config);
